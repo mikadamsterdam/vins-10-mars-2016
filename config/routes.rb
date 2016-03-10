a7db4d2d2cfc8716dded05b73e47724c75ae377f
mikadamsterdam <mikadamsterdam@concepts.nl>
2016-03-10 13:52:31 +0100

    routes, nested,etc

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :reviews
-  resources :wines
+  resources :wines do
+    resources :reviews
+end
+
+root to:"wines#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
